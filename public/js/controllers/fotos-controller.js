360f95b3888e00fd60db6eb20f7b4ee507a0cdff
Ezequias <ezequias.prado@webmaissistemas.com.br>
2021-01-26 21:45:15 -0300

    first commit

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,15 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http){
+    $scope.fotos = [];
+    /*var promisse = $http.get('v1/fotos');
+    promisse.then(function(retorno){
+        $scope.fotos = retorno.data;
+    }).catch(function(error){
+        console.log(error);
+    });*/
+    $http.get('v1/fotos')
+    .success(function(fotos){
+        $scope.fotos = fotos;
+    }).error(function(erro){
+        console.log(erro);
+    });
+});
